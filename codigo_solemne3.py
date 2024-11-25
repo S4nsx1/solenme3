2a3deaf0f7e95c037aaad6423f9e0fe7ff132d14
S4nsx1 <benjamincancino90@gmail.com>
2024-11-25 14:07:00 -0300

    Update codigo_solemne3.py

--- codigo_solemne3.py
+++ codigo_solemne3.py
@@ -3,8 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("valorant champions istanbul.csv")
-
-if st.button("K/D promedio por equipos"):
+a_columns , b_columns = st.columns(2)
+if a_columns.button("K/D promedio por equipos"):
   fig = plt.figure(figsize=(10, 6))
   kd = df.groupby('Team')['K/D'].mean().sort_values(ascending=False)
   plt.bar(kd.index,kd.values,color="purple")
@@ -14,7 +14,7 @@ if st.button("K/D promedio por equipos"):
   _ = plt.xticks(rotation="horizontal", ha='right')
   st.pyplot(fig)
   
-if st.button("Jugador con mas kills"):
+if b_columns.button("Jugador con mas kills"):
   fig = plt.figure(figsize=(12, 6))
   kills = df.groupby('Player')['Kill'].mean().sort_values(ascending=False)
   plt.bar(kills.index, kills.values)
